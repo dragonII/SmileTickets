6764ecafba2b17410a5dc5aca1b4a0c640ac1cc0
dragonII <31319070@qq.com>
2015-02-06 16:22:29 +0800

    Initial Commit

---
+++ SmileTickets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmileTickets
+//
+//  Created by Wang Long on 2/6/15.
+//  Copyright (c) 2015 Wang Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
